25de418632b5000c937d49b4e22a8d9e260a5889
Kristian Tamm <tammkristian@gmail.com>
2024-11-26 14:27:27 +0200

    updated elevator detector

--- elevator_door_detector.py
+++ elevator_door_detector.py
@@ -6,6 +6,7 @@ import collections
 from PIL import Image
 import os
 import urllib.request
+from tensorflow.keras.models import load_model
 
 
 class ElevatorDoorDetector(object):
@@ -14,8 +15,9 @@ class ElevatorDoorDetector(object):
 
         #self.STOP_SIGN_CLASS_ID = 12
         self.min_score = min_score
-        self.show_bounding_box = show_bounding_box
         self.debug = debug
+        self.model_name = 'liftModel'
+        self.model = load_model('mycar/models/lightweight_cnn_model.h5')
 
         # reverse throttle related
         self.max_reverse_count = max_reverse_count
@@ -30,17 +32,23 @@ class ElevatorDoorDetector(object):
     
 
     def detect_elevator_door (self, img_arr):
-        return None
+        img = self.convertImageArrayToPILImage(img_arr)
+        # Model inference
+        prediction = self.model.predict(img)
+        predicted_class = np.argmax(prediction, axis=1)
+        return predicted_class == 1
 
 
     def run(self, img_arr, throttle, debug=False):
         if img_arr is None:
             return throttle, img_arr
-        if self.reverse_count < self.max_reverse_count:
-            self.is_reversing = True
-            self.reverse_count += 1
+        if self.detect_elevator_door(img_arr):
+            #self.is_reversing = True
+            #self.reverse_count += 1
+            print('n-sõna')
             return self.reverse_throttle, img_arr
         else:
+            print('faaakkk')
             self.is_reversing = False
             self.reverse_count = 0
             return throttle, img_arr
