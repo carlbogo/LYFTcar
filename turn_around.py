a6feda184bd2e90e4c5d2ce730ab745b2624dcb4
Kristian Tamm <tammkristian@gmail.com>
2025-01-07 17:41:04 +0200

    bimbaam

--- turn_around.py
+++ turn_around.py
@@ -29,8 +29,8 @@ class TurnAround:
         self.current_pivot = 0
         self.enabled = False
 
-    def launch():
-        print('Juhhuuuu!. Pööramini töötab!')
+    def launch(self):
+        self.enabled = not self.enabled
 
     def run(self, throttle, steering):
         """
@@ -43,53 +43,54 @@ class TurnAround:
         Returns:
             tuple: (throttle, steering) values to control the car.
         """
-        current_time = time.time()
+        if self.enabled:
+            current_time = time.time()
 
-        # Start timing for the first state
-        if self.start_time is None:
-            self.start_time = current_time
-
-        # Reverse straight
-        if self.state == "reverse":
-            if current_time - self.start_time < self.reverse_duration:
-                return self.reverse_throttle, 0.0
-            else:
-                self.state = "pivot_left"
+            # Start timing for the first state
+            if self.start_time is None:
                 self.start_time = current_time
 
-        # Pivot left (turn left in place)
-        elif self.state == "pivot_left":
-            if current_time - self.start_time < self.pivot_duration:
-                return 0.0, -1.0  # Zero throttle, max left steering
-            else:
-                self.state = "forward"
-                self.start_time = current_time
-
-        # Move forward slightly
-        elif self.state == "forward":
-            if current_time - self.start_time < self.reverse_duration:
-                return self.forward_throttle, 0.0
-            else:
-                self.state = "pivot_right"
-                self.start_time = current_time
-
-        # Pivot right (turn right in place)
-        elif self.state == "pivot_right":
-            if current_time - self.start_time < self.pivot_duration:
-                return 0.0, 1.0  # Zero throttle, max right steering
-            else:
-                self.current_pivot += 1
-                if self.current_pivot >= self.pivot_count:
-                    self.state = "done"
+            # Reverse straight
+            if self.state == "reverse":
+                if current_time - self.start_time < self.reverse_duration:
+                    return self.reverse_throttle, 0.0
                 else:
-                    self.state = "reverse"
-                self.start_time = current_time
+                    self.state = "pivot_left"
+                    self.start_time = current_time
+
+            # Pivot left (turn left in place)
+            elif self.state == "pivot_left":
+                if current_time - self.start_time < self.pivot_duration:
+                    return 0.0, -1.0  # Zero throttle, max left steering
+                else:
+                    self.state = "forward"
+                    self.start_time = current_time
+
+            # Move forward slightly
+            elif self.state == "forward":
+                if current_time - self.start_time < self.reverse_duration:
+                    return self.forward_throttle, 0.0
+                else:
+                    self.state = "pivot_right"
+                    self.start_time = current_time
+
+            # Pivot right (turn right in place)
+            elif self.state == "pivot_right":
+                if current_time - self.start_time < self.pivot_duration:
+                    return 0.0, 1.0  # Zero throttle, max right steering
+                else:
+                    self.current_pivot += 1
+                    if self.current_pivot >= self.pivot_count:
+                        self.state = "done"
+                    else:
+                        self.state = "reverse"
+                    self.start_time = current_time
 
         # Done state
         elif self.state == "done":
             return 0.0, 0.0  # Stop movement
 
-        return 0.0, 0.0  # Default return if something goes wrong
+        return throttle, steering  # Default return if something goes wrong
 
     def shutdown(self):
         """Clean up resources when shutting down."""
