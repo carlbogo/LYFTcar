0727f6353b27b93c2379cad4b4ca70e239fd018b
Kristian Tamm <tammkristian@gmail.com>
2025-01-13 19:38:36 +0200

    latest turnAround :)

--- turn_around.py
+++ turn_around.py
@@ -8,7 +8,7 @@
 import time
 
 class TurnAround:
-    def __init__(self, pivot_duration=1.5, reverse_duration=1.0, forward_throttle=0.3, reverse_throttle=-0.3, pivot_count=2):
+    def __init__(self, pivot_duration=1.5, reverse_duration=1.0, forward_throttle=0.85, reverse_throttle=-0.85, pivot_count=3):
         """
         Initialize the TightTurn part.
 
@@ -30,6 +30,7 @@ class TurnAround:
         self.enabled = False
 
     def launch(self):
+        print('turnAround launched!')
         self.enabled = not self.enabled
 
     def run(self, throttle, steering):
@@ -43,52 +44,60 @@ class TurnAround:
         Returns:
             tuple: (throttle, steering) values to control the car.
         """
-        if self.enabled:
-            current_time = time.time()
+        if not self.enabled:
+            return throttle, steering
+        
+        print('turnAround - start')
 
-            # Start timing for the first state
-            if self.start_time is None:
+        current_time = time.time()
+
+        # Start timing for the first state
+        if self.start_time is None:
+            self.start_time = current_time
+
+        # Reverse straight
+        if self.state == "reverse":
+            if current_time - self.start_time < self.reverse_duration:
+                print('reversing...')
+                return self.reverse_throttle, 0.0
+            else:
+                self.state = "pivot_left"
                 self.start_time = current_time
 
-            # Reverse straight
-            if self.state == "reverse":
-                if current_time - self.start_time < self.reverse_duration:
-                    return self.reverse_throttle, 0.0
-                else:
-                    self.state = "pivot_left"
-                    self.start_time = current_time
+        # Pivot left (turn left in place)
+        elif self.state == "pivot_left":
+            if current_time - self.start_time < self.pivot_duration:
+                print('turn left')
+                return 0.0, -1.0  # Zero throttle, max left steering
+            else:
+                self.state = "forward"
+                self.start_time = current_time
 
-            # Pivot left (turn left in place)
-            elif self.state == "pivot_left":
-                if current_time - self.start_time < self.pivot_duration:
-                    return 0.0, -1.0  # Zero throttle, max left steering
-                else:
-                    self.state = "forward"
-                    self.start_time = current_time
+        # Move forward slightly
+        elif self.state == "forward":
+            if current_time - self.start_time < self.reverse_duration:
+                print('goin forward')
+                return self.forward_throttle, 0.0
+            else:
+                self.state = "pivot_right"
+                self.start_time = current_time
 
-            # Move forward slightly
-            elif self.state == "forward":
-                if current_time - self.start_time < self.reverse_duration:
-                    return self.forward_throttle, 0.0
+        # Pivot right (turn right in place)
+        elif self.state == "pivot_right":
+            if current_time - self.start_time < self.pivot_duration:
+                print('turnin right')
+                return 0.0, 1.0  # Zero throttle, max right steering
+            else:
+                self.current_pivot += 1
+                if self.current_pivot >= self.pivot_count:
+                    self.state = "done"
                 else:
-                    self.state = "pivot_right"
-                    self.start_time = current_time
-
-            # Pivot right (turn right in place)
-            elif self.state == "pivot_right":
-                if current_time - self.start_time < self.pivot_duration:
-                    return 0.0, 1.0  # Zero throttle, max right steering
-                else:
-                    self.current_pivot += 1
-                    if self.current_pivot >= self.pivot_count:
-                        self.state = "done"
-                    else:
-                        self.state = "reverse"
-                    self.start_time = current_time
+                    self.state = "reverse"
+                self.start_time = current_time
 
         # Done state
         elif self.state == "done":
-            return 0.0, 0.0  # Stop movement
+            self.enabled = False
 
         return throttle, steering  # Default return if something goes wrong
 
